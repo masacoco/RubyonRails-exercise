2a3a61c96519e1aa59629751e660528aac636741
EC2 Default User <ec2-user@ip-172-31-87-86.ec2.internal>
2021-08-06 05:42:55 +0000

    [ADD]deleting

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,10 +4,14 @@ class TodolistsController < ApplicationController
   end
 
 def create
-  list=List.new(list_params)
-  list.save
-  redirect_to todolist_path(list.id)
-end
+  
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to todolist_path(@list.id)
+    else
+      render :new
+    end 
+ end
 
 def index
   @lists = List.all
@@ -27,9 +31,17 @@ def update
   redirect_to todolist_path(list.id)
 end  
 
+def destroy
+    list = List.find(params[:id])  # データ（レコード）を1件取得
+    list.destroy  # データ（レコード）を削除
+    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+end  
+
+
 private
 def list_params
-  params.require(:list).permit(:title,:body)
+  params.require(:list).permit(:title,:body,:image)
+  
 end
 
 end
